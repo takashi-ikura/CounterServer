4752627e4a969b68a12005c1cf69d9a7dfd2e590
takashi-ikura <takashi_ikura@marugo.com>
2026-05-14 16:07:18 +0900

    不要ソースを削除

--- web/app.py
+++ web/app.py
@@ -1,8 +1,6 @@
-import sqlite3
 import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from flask import jsonify
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -19,12 +17,6 @@ def counter():
 @app.route('/home')
 def home():
     return render_template('home.html')
-
-@app.route('/api/notify_update')
-def notify_update():
-    # 接続中の全ブラウザに 'update_event' を送る
-    socketio.emit('update_event', {'message': 'refresh_needed'})
-    return "OK"
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=PORT_NO)
